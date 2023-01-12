c98c2e24c474e2949fa77323b903b48deb651c4f
sudhacoolcoder <104515063+sudhacoolcoder@users.noreply.github.com>
2023-01-12 19:06:11 +0530

    Add files via upload

---
+++ bed_room.js
@@ -0,0 +1,42 @@
+img = "";
+status ="";
+
+function preload()
+{
+    img = loadImage('bed_room.png');
+}
+
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status: Detecting objects";
+
+
+
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded !")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+
+    }
+    console.log(results);
+}
---
+++ bottle.js
@@ -0,0 +1,42 @@
+img = "";
+status ="";
+
+function preload()
+{
+    img = loadImage('bottle.jpeg');
+}
+
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status: Detecting objects";
+
+
+
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded !")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+
+    }
+    console.log(results);
+}
